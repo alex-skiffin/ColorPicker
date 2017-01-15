d102448e94ee595e3d9d1a6aade616f631e9374c
alex-skiffin <alex@skiffin.ru>
2017-01-15 15:25:24 +0500

    * связал инпуты с результатом

--- app/picker/inputs/inputs.component.ts
+++ app/picker/inputs/inputs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'inputs',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
     styleUrls: ['app/picker/inputs/inputs.component.css']
 })
 export class InputsComponent {
+    public colorR: string;
 
+    public colorG: string;
+
+    public colorB: string;
+
+    private validateInput(value: string): string {
+        if(!value)
+            return '00';
+
+        value = value.replace(/[^A-Fa-f0-9]/g, "");
+        
+        if(value.length > 2)
+            value=value.substring(value.length-2);
+            
+        if(value.length===0)
+            value='00';
+        if(value.length===1)
+            value='0'+value;
+            
+        return value;
+    }
 }
--- app/picker/picker.component.ts
+++ app/picker/picker.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { InputsComponent } from './inputs/inputs.component';
+import { PickerAreaComponent } from './picker-area/area.component';
+import { RulerComponent } from './ruler/ruler.component';
 
 @Component({
     selector: 'color-picker',
@@ -6,4 +9,28 @@ import { Component } from '@angular/core';
     styleUrls: [ 'app/picker/picker.component.css' ]
 })
 
-export class ColorPickerComponent {}
+export class ColorPickerComponent {
+    @ViewChild(InputsComponent)
+    private inputs: InputsComponent;
+
+    @ViewChild(PickerAreaComponent)
+    private area: PickerAreaComponent;
+
+    @ViewChild(RulerComponent)
+    private ruler: RulerComponent;
+
+    private color(): string {
+        return '#'
+        + this.inputs.colorR
+        + this.inputs.colorG
+        + this.inputs.colorB;
+    };
+
+    public ngOnInit(): void {
+        this.inputs.colorR='00';
+        this.inputs.colorG='00';
+        this.inputs.colorB='00';
+
+        this.ruler.setGradient();
+    }
+}
